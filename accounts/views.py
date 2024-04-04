5ad471324c207f5c629f9c3da3121cdfe7ceaa5c
박수빈 <subin57419@gmail.com>
2024-04-04 16:16:36 +0900

    회원탈퇴 기능 생성

--- accounts/views.py
+++ accounts/views.py
@@ -37,3 +37,8 @@ def signup(request):
         'form': form,
     }
     return render(request, 'accounts/signup.html', context)
+
+
+def delete(request):
+    request.user.delete()
+    return redirect('articles:index')
