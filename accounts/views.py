3af3fd060f58d56d114b8e5d03c5237b5ae062de
박수빈 <subin57419@gmail.com>
2024-04-04 16:53:49 +0900

    회원가입 후 로그인 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -36,7 +36,8 @@ def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request, user)
             return redirect('articles:index')
     else :
         form = CustomUserCreationForm()
