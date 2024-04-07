b69da7646ade6f87cef863b9c7cd4599ef0fd7fa
박수빈 <subin57419@gmail.com>
2024-04-07 14:16:57 +0900

    웹에 표 만들기

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
 
 # Create your views here.
+def index(request):
+    csv_path = 'cyber_crimes.csv'
+    df = pd.read_csv(csv_path, encoding = 'euc-kr')
+    context = {
+        'df': df,
+    }
+    return render(request, 'pages/index.html', context)
